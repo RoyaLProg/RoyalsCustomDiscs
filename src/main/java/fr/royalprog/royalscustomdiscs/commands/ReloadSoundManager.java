24f5c0fca2a16cb13f5dc8ea0dd1564a20e2248b
Camille CAMBIUM <royalfr@protonmail.com>
2024-04-27 18:22:46 +0200

    NEW : adding a command to reload sounds
    EXPERIMENTALISATION

--- src/main/java/fr/royalprog/royalscustomdiscs/commands/ReloadSoundManager.java
+++ src/main/java/fr/royalprog/royalscustomdiscs/commands/ReloadSoundManager.java
@@ -3,20 +3,19 @@ package fr.royalprog.royalscustomdiscs.commands;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.client.Minecraft;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 
 public class ReloadSoundManager {
-	public static void register(CommandDispatcher<CommandSourceStack> dispatcher){
+	public ReloadSoundManager(CommandDispatcher<CommandSourceStack> dispatcher){
 		dispatcher.register(Commands.literal("reloadsounds").executes(ReloadSoundManager::execute));
 	}
 
 	private static int execute(CommandContext<CommandSourceStack> command){
-		if(command.getSource().getEntity() instanceof Player player) {
-			player.sendSystemMessage(Component.literal("Hello World")); // Test response
-		}
+		Minecraft.getInstance().getSoundManager().reload();
 		return Command.SINGLE_SUCCESS;
 	}
 }
--- src/main/java/fr/royalprog/royalscustomdiscs/events/ModEvents.java
+++ src/main/java/fr/royalprog/royalscustomdiscs/events/ModEvents.java
@@ -1,2 +1,14 @@
-package fr.royalprog.royalscustomdiscs.events;public class ModEvents {
+package fr.royalprog.royalscustomdiscs.events;
+
+import fr.royalprog.royalscustomdiscs.RoyaLsCustomDiscs;
+import fr.royalprog.royalscustomdiscs.commands.ReloadSoundManager;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+@Mod.EventBusSubscriber(modid = RoyaLsCustomDiscs.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ModEvents {
+	@SubscribeEvent
+	public static void onCommandRegister(RegisterCommandsEvent event) {
+		new ReloadSoundManager(event.getDispatcher());
+	}
 }
